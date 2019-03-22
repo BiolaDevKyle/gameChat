739494bb601ee4617b9c60a2f1cefee2bef9bed6
Kyle Rhodes <kyle.rhodes@biola.edu>
2019-03-22 11:54:43 -0700

    file structure plus home and user controllers

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const homepage = require('./application/homepage.js');
+const userManagement = require('./application/userManagement.js');
 
 const app = express();
 
-  app.get('/', function(req, res){
-    res.send('<h1> Welcome to the greatest site ever </h1>');
-  })
+  homepage(app);
+  userManagement(app);
 
 app.listen(3001, function(){
   console.log('listening on localhost:3001');
